3cdfcd0f5b5deb68c3be920ddd72a933f34c38cf
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-03-30 16:16:30 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Steps to create the desktop app
+
+### Install dependencies
+npm install
+
+### Run command builder
+npm run build-installer
+
+This will create a dist folder with the .exe file installer inside
+
+If this command gives you an error and doesn't generate the .exe file, 
+try to execute the command using cmd as an administrator
+
---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const os = require('os-utils')
+
+const createWindow = () => {
+
+const url = "https://kinema-ms.vercel.app"
+
+console.log('HOLA?', __dirname + '/icon.ico')
+
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    icon: path.join(__dirname, '/public/icon.ico'),
+    // icon: __dirname + '/icon.ico',
+    autoHideMenuBar: true,
+    title: 'KINEMÁ',
+  })
+  win.loadURL(url)
+
+  os.cpuUsage((v) => {
+    console.log('CPU Usage (%): ', v*100)
+    console.log('Mem Usage (%): ', os.freememPercentage()*100)
+    console.log('Total Usage (GB): ', os.totalmem()/1024)
+  })
+
+  // win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
